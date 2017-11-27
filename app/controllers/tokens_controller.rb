ff36213b09b3510b613501b70dbdd887fe1543ef
KASHISH <kaggarw2@ncsu.edu>
2017-11-27 01:09:55 -0500

    encryot/decrypt token json

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,45 +1,40 @@
 class TokensController < ApplicationController
   skip_before_action :verify_authenticity_token
-  def getToken
 
+  def getToken
     token = Token.new
     token.client_id = params[:clientid]
-
-    cipher = OpenSSL::Cipher::AES256.new(:CBC)
+    cipher = OpenSSL::Cipher::AES.new(128, :CBC)
     cipher.encrypt
     keysindb = Key.all
     if keysindb.size == 0
       newkey = Key.new
-      newkey.key = Base64.encode64(cipher.random_key).encode('utf-8')
-      newkey.initial_value = Base64.encode64(cipher.random_iv).encode('utf-8')
-      puts newkey
+      newkey.key = Base64.encode64(cipher.random_key)
+      newkey.initial_value = Base64.encode64(cipher.random_iv)
       newkey.save
     else
       oldkey = keysindb.reverse[0]
-      cipher.key = oldkey.key
-      cipher.iv = oldkey.initial_value
+      cipher.key = Base64.decode64(oldkey.key)
+      cipher.iv = Base64.decode64(oldkey.initial_value)
     end
-    puts token.to_json
     data = "confidential data"
-    token.token = Base64.encode64(cipher.update(token.to_json) + cipher.final).encode('utf-8')
+    token.token = Base64.encode64(cipher.update(token.to_json) + cipher.final)
     render json: token
   end
 
   def validateToken
-    puts params["token"]
-    token = Base64.decode64(params["token"]).encode('ascii-8bit')
-    keysindb = Key.all
-    decipher = OpenSSL::Cipher::AES256.new(:CBC)
-    decipher.decrypt
-    decipher.padding = 0
-    oldkey = keysindb.reverse[0]
-    puts oldkey.key
-    decipher.key = Base64.decode64(oldkey.key).encode('ascii-8bit')
-    decipher.iv = Base64.decode64(oldkey.initial_value).encode('ascii-8bit')
-    plain = decipher.update(token) + decipher.final
+    token = Base64.decode64(params["token"])
 
+    keysindb = Key.all
+    decipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    decipher.decrypt
+    oldkey = keysindb.reverse[0]
+    decipher.key = Base64.decode64(oldkey.key)
+    decipher.iv = Base64.decode64(oldkey.initial_value)
+    plain = decipher.update(token) + decipher.final
+    puts plain
     res = ValidationResponse.new
-    res.clientid = 123
+    res.clientname = plain
     render json: res
   end
 end
