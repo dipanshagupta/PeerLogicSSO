e036e58cc5d50a911716e37b3ca93dcad83c8bbb
Dipansha <dipansha@Dipanshas-MacBook-Pro.local>
2017-11-30 20:29:29 -0500

    Corrected route

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -82,7 +82,7 @@ class ClientsController < ApplicationController
     @client.user_id = @current_user.id
     respond_to do |format|
       if @client.save
-        format.html { redirect_to @client, notice: 'Client was successfully created.' }
+        format.html { redirect_to clients_path, notice: 'Client was successfully created.' }
         format.json { render :show, status: :created, location: @client }
       else
         format.html { render :new }
